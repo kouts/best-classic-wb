7befdf9b72877cb27aaad9055efcee629d17bb6d
kouts <koutsaftakis@gmail.com>
2024-02-03 15:27:54 +0200

    fix: updated github action

--- release.config.js
+++ release.config.js
@@ -20,7 +20,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "dist", "coverage"],
+        assets: ["CHANGELOG.md", "BestClassicWB.zip"],
         message:
           "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
